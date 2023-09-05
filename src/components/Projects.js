c5918ec6517f6ce6d70718d8ed80a023cf246820
Yossijaki <yossijaki@gmail.com>
2023-09-04 22:46:10 -0600

    Projects tabs created

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,83 @@
-export const Projects = () => {
+import { Col, Container, Row } from "react-bootstrap";
 
+export const Projects = () => {
     const projects = [
-        
-    ];
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+  ];
+
     return (
-        
-    )
-}
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Architecto vel et dolore reiciendis, nihil laborum!</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Row>
+                                <Col sm={3}>
+                                    <Nav variant="pills" className="flex-column">
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                        </Nav.Item>
+                                    </Nav>
+                                </Col>
+                                <Col sm={9}>
+                                    <Tab.Content>
+                                        <Tab.Pane eventKey="first">
+                                            <Row>
+                                                {
+                                                    projects.map(( project, index ) => {
+                                                        return (
+                                                            <p>{ project.title }</p>
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+                                        <Tab.Pane eventKey="third">Third tab content</Tab.Pane>
+                                    </Tab.Content>
+                                </Col>
+                            </Row>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
