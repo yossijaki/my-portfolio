e93f80aa2be02fdde1bc5ad048ab0752a4af9723
Yossijaki <yossijaki@gmail.com>
2023-09-05 20:07:36 -0600

    Projects tabs and Projects text Fixed

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
         <Col sm={6} md={4}>
             <div className="proj-imgbx">
                 <img src={imgUrl} />
-                <div className="proj-txt">
+                <div className="proj-txtx">
                     <h4>{ title }</h4>
                     <span>{ description }</span>
                 </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -47,41 +47,35 @@ export const Projects = () => {
                         <h2>Projects</h2>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Architecto vel et dolore reiciendis, nihil laborum!</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Row>
-                                <Col sm={3}>
-                                    <Nav variant="pills" className="flex-column">
-                                        <Nav.Item>
-                                            <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                                        </Nav.Item>
-                                        <Nav.Item>
-                                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                                        </Nav.Item>
-                                        <Nav.Item>
-                                            <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                                        </Nav.Item>
-                                    </Nav>
-                                </Col>
-                                <Col sm={9}>
-                                    <Tab.Content>
-                                        <Tab.Pane eventKey="first">
-                                            <Row>
-                                                {
-                                                    projects.map(( project, index ) => {
-                                                        return (
-                                                            <ProjectCard
-                                                                key={index}
-                                                                {...project}
-                                                            />
-                                                        )
-                                                    })
-                                                }
-                                            </Row>
-                                        </Tab.Pane>
-                                        <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
-                                        <Tab.Pane eventKey="third">Third tab content</Tab.Pane>
-                                    </Tab.Content>
-                                </Col>
-                            </Row>
+                            <Nav variant="pills" defaultActiveKey="/home">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map(( project, index ) => {
+                                                return (
+                                                    <ProjectCard
+                                                        key={index}
+                                                        {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+                                <Tab.Pane eventKey="third">Third tab content</Tab.Pane>
+                            </Tab.Content>
                         </Tab.Container>
                     </Col>
                 </Row>
