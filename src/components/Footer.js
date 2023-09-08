61f4c94ac8c6dcd6758dd4985ecd278a2505ecce
Yossijaki <yossijaki@gmail.com>
2023-09-07 23:26:34 -0600

    added components to app.js and quickfixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,26 @@
-import { Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import { MailchimpForm } from "./MailchimpForm"
-import { logo } from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const Footer = () => {
     return (
         <footer className="footer">
             <Container>
-                <Row>
-                    <MailchimpForm/>
+                <Row className="align-item-center">
+                    <MailchimpForm />
                     <Col sm={6}>
                         <img src={logo} alt="logo" />
+                    </Col>
+                    <Col sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href=""><img src={navIcon1} /></a>
+                            <a href=""><img src={navIcon2} /></a>
+                            <a href=""><img src={navIcon3} /></a>
+                        </div>
+                        <p>Copyright 2022. All rights reserved by Yossijaki</p>
                     </Col>
                 </Row>
             </Container>
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -8,7 +8,7 @@ export const Newsletter = ({ onValidated, status, message }) => {
         if (status === 'success') clearFields()
     }, [status])
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         email &&
         email.indexOf("@") > -1 &&
