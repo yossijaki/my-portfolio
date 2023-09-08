115f2254ece1b768ea4c3a980f63ecf7088ec857
Yossijaki <yossijaki@gmail.com>
2023-09-08 14:08:21 -0600

    Animations added to Banner and Projects sections along with its dependencies and footer CSS bugfixes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,9 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg"
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
@@ -11,12 +13,12 @@ export const Banner = () => {
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
 
-    useEffect(()=> {
+    useEffect(() => {
         let ticker = setInterval(() => {
             tick();
         }, delta)
 
-        return () => { clearInterval(ticker)};
+        return () => { clearInterval(ticker) };
     }, [text])
 
     const tick = () => {
@@ -45,10 +47,15 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hey! I'm Yossijaki, Software `}<span className="wrap">{text}</span></h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__fadeInUp" : ""}>
+                                    <span className="tagline">Welcome to my Portfolio</span>
+                                    <h1>{`Hey! I'm Yossijaki, Software `}<span className="wrap">{text}</span></h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                                </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Image" />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,86 +1,91 @@
 import { Col, Container, Row, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import projImg1 from "../assets/img/project-img1.png"
-import projImg2 from "../assets/img/project-img2.png"
-import projImg3 from "../assets/img/project-img3.png"
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
-    const projects = [
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-        },
-  ];
+	const projects = [
+		{
+			title: "Business Startup",
+			description: "Design & Development",
+			imgUrl: projImg1,
+		},
+		{
+			title: "Business Startup",
+			description: "Design & Development",
+			imgUrl: projImg2,
+		},
+		{
+			title: "Business Startup",
+			description: "Design & Development",
+			imgUrl: projImg3,
+		},
+		{
+			title: "Business Startup",
+			description: "Design & Development",
+			imgUrl: projImg1,
+		},
+		{
+			title: "Business Startup",
+			description: "Design & Development",
+			imgUrl: projImg2,
+		},
+		{
+			title: "Business Startup",
+			description: "Design & Development",
+			imgUrl: projImg3,
+		},
+	];
 
-    return (
-        <section className="project" id="project">
-            <Container>
-                <Row>
-                    <Col>
-                        <h2>Projects</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Architecto vel et dolore reiciendis, nihil laborum!</p>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                            <Tab.Content>
-                                <Tab.Pane eventKey="first">
-                                    <Row>
-                                        {
-                                            projects.map(( project, index ) => {
-                                                return (
-                                                    <ProjectCard
-                                                        key={index}
-                                                        {...project}
-                                                    />
-                                                )
-                                            })
-                                        }
-                                    </Row>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
-                                <Tab.Pane eventKey="third">Third tab content</Tab.Pane>
-                            </Tab.Content>
-                        </Tab.Container>
-                    </Col>
-                </Row>
-            </Container>
-            <img className="background-image-right" src={colorSharp2}></img>
-        </section>
-    );
+	return (
+		<section className="project" id="project">
+			<Container>
+				<Row>
+					<Col>
+						<TrackVisibility>
+							{({ isVisible }) => 
+								<div className={ isVisible ? "animate__animated animate__fadeInUp" : ""}>
+									<h2>Projects</h2>
+									<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Architecto vel et dolore reiciendis, nihil laborum!</p>
+								</div>
+							}
+						</TrackVisibility>
+						<Tab.Container id="projects-tabs" defaultActiveKey="first">
+							<Nav
+								variant="pills"
+								className="nav-pills mb-5 justify-content-center align-items-center"
+								id="pills-tab"
+							>
+								<Nav.Item>
+									<Nav.Link eventKey="first">Tab 1</Nav.Link>
+								</Nav.Item>
+								<Nav.Item>
+									<Nav.Link eventKey="second">Tab 2</Nav.Link>
+								</Nav.Item>
+								<Nav.Item>
+									<Nav.Link eventKey="third">Tab 3</Nav.Link>
+								</Nav.Item>
+							</Nav>
+							<Tab.Content>
+								<Tab.Pane eventKey="first">
+									<Row>
+										{projects.map((project, index) => {
+											return <ProjectCard key={index} {...project} />;
+										})}
+									</Row>
+								</Tab.Pane>
+								<Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+								<Tab.Pane eventKey="third">Third tab content</Tab.Pane>
+							</Tab.Content>
+						</Tab.Container>
+					</Col>
+				</Row>
+			</Container>
+			<img className="background-image-right" src={colorSharp2}></img>
+		</section>
+	);
 };
