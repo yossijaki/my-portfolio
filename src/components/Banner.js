525e1965fa9e876d0b9de3b379864d12ae8e86d3
Yossijaki <yossijaki@gmail.com>
2023-09-09 17:49:39 -0600

    Type animation changed and some css changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,47 +1,11 @@
-import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg"
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { TypeAnimation } from 'react-type-animation';
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Front Dev.", "Back Dev.", "FullStack Dev."];
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const period = 2000;
-
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta)
-
-        return () => { clearInterval(ticker) };
-    }, [text])
-
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-        setText(updatedText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setDelta(500);
-        }
-    }
-
     return (
         <section className="banner" id="home">
             <Container>
@@ -51,7 +15,26 @@ export const Banner = () => {
                             {({ isVisible }) =>
                                 <div className={isVisible ? "animate__animated animate__fadeInUp" : ""}>
                                     <span className="tagline">Welcome to my Portfolio</span>
-                                    <h1>{`Hey! I'm Yossijaki, Software `}<span className="wrap">{text}</span></h1>
+                                    <h1>{`Hey! I'm Yossijaki, `}</h1>
+                                    <TypeAnimation 
+                                            sequence={[
+                                                'Software Developer', // Types 'One'
+                                                2000, // Waits 2s
+                                                'Front D', // Deletes 'One' and types 'Two'
+                                                300, // Waits 3ms
+                                                'Backend Dev',
+                                                300, // Waits 3ms
+                                                'FullStack Developer',
+                                                2000, // Waits 2s
+                                                () => {
+                                                    console.log('Sequence completed');
+                                                },
+                                            ]}
+                                            wrapper="span"
+                                            cursor={true}
+                                            repeat={Infinity}
+                                            style={{ fontSize: '65px', display: 'inline-block' }}
+                                        />
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                                     <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
                                 </div>}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,14 +9,14 @@ import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, seScrolled] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
         const onScroll = () => {
             if (window.scrollY > 50) {
-                seScrolled(true);
+                setScrolled(true);
             } else {
-                seScrolled(false);
+                setScrolled(false);
             }
         }
 
