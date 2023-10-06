57772a73237e6cd601241757b8757daf7752ad9b
Yossijaki <yossijaki@gmail.com>
2023-10-05 20:07:32 -0600

    UnderConstruction component modified: title added

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -4,7 +4,8 @@ import WorkInProgress from "../assets/img/worki-in-progress.svg"
 export const UnderConstruction = () => {
     return (
         <Col sm={12} md={12}>
-            <div className="Work-in-progress">
+            <div className="work-in-progress">
+                <h3>This section is under construction</h3>
                 <img src={WorkInProgress} />
             </div>
         </Col>
